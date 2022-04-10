eac92cfe5e78f38606e25e781abcea8c7fa9b7db
Admin <kostinukg@gmail.com>
2022-04-10 17:45:25 +0300

    Ашыпкию

--- 1000000.py
+++ 1000000.py
@@ -1,3 +1,13 @@
-print("Hello World!")
-name = input("Как тебя зовут? ")
-print(f"Привет, {name}!")
+a = int(input("Ведите первое чысло: "))
+
+try:
+    b = int(input("Ведите второе чысло: "))
+    print(a / b)
+except ZeroDivisionError:
+    print("error zero")
+except Exception as error:
+    print(f"ff {error}")
+else:
+    print("ffffff")
+finally:
+    print("gffffgfffg")
